1d208f49b98b83353b3a4564516b4e0906362a32
Jacob Lefler <jcb.lefler@gmail.com>
2019-05-14 15:25:28 -0400

    added update/details/delete fuction

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -33,6 +34,24 @@ public class HomeController {
             return "todoform";
         }
         toDoRepository.save(todo);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/details/{id}")
+    public String showToDo(@PathVariable("id") long id, Model model){
+        model.addAttribute("todo", toDoRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateToDo(@PathVariable("id") long id, Model model){
+        model.addAttribute("todo", toDoRepository.findById(id).get());
+        return "todoform";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delToDo(@PathVariable("id") long id){
+        toDoRepository.deleteById(id);
         return "redirect:/";
     }
 }
